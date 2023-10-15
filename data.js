d64bf543faea404db6e8b9baf565501a354409c7
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-10-15 22:06:43 +0530

    First Project of website

---
+++ data.js
@@ -0,0 +1,21 @@
+let ecomData = [
+  {
+    id: 1,
+    image:
+      "https://www.chimpwear.com/pub/media/catalog/product/cache/926507dc7f93631a094422215b778fe0/m/u/mumbai-open-mens-1.jpg",
+    brandName: "HRX",
+    type: "T-shirt",
+    price: "599",
+    count: 0,
+  },
+  {
+    id: 2,
+    image: "https://m.media-amazon.com/images/I/81rMUtlAR6L._AC_UY1100_.jpg",
+    brandName: "jockey",
+    type: "shorts",
+    price: "899",
+    count: 0,
+  },
+];
+
+export { ecomData };
