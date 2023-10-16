90ade32632fc5ac37a996bc161ba3c9e2b49dfdf
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2023-10-16 14:33:40 +0530

    New project

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 let ecomData = [
+  // T-SHIRT
   {
     id: 1,
     image:
@@ -10,10 +11,448 @@ let ecomData = [
   },
   {
     id: 2,
+    image:
+      "https://www.jiomart.com/images/product/original/rvowvf0akl/eyebogler-men-s-polo-neck-regular-fit-half-sleeves-colorblocked-teal-t-shirt-product-images-rvowvf0akl-0-202211051905.jpg?im=Resize=(500,630)",
+    brandName: "eyebolger-men",
+    type: "T-shirt",
+    price: "599",
+    count: 0,
+  },
+  {
+    id: 3,
+    image:
+      "https://www.triumph-sports.com/wp-content/uploads/2020/10/WarCycTshirt12-1-1.jpg",
+    brandName: "Triumph",
+    type: "T-shirt",
+    price: "499",
+  },
+  {
+    id: 4,
+    image:
+      "https://static.wixstatic.com/media/c02b09_39ce34f017984072b0a48e72dd2a15fd~mv2.jpg/v1/fill/w_526,h_526,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c02b09_39ce34f017984072b0a48e72dd2a15fd~mv2.jpg",
+    brandName: "vectormantra",
+    type: "T-shirt",
+    price: "599",
+    count: 0,
+  },
+
+  {
+    id: 5,
+    image:
+      "https://mirzacdns3.s3.ap-south-1.amazonaws.com/cache/catalog/RHP1096/1-445x618.jpg",
+    brandName: "Redtape",
+    type: "T-shirt",
+    price: "766",
+    count: 0,
+  },
+  // SHORTS
+
+  {
+    id: 6,
     image: "https://m.media-amazon.com/images/I/81rMUtlAR6L._AC_UY1100_.jpg",
     brandName: "jockey",
     type: "shorts",
     price: "899",
+    count: 0,
+  },
+  {
+    id: 7,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1oOJqEolU-eaz4_jjoCtUlPjFbyGggEeuPw&usqp=CAU",
+    brandName: "stoney creek",
+    type: "shorts",
+    price: "399",
+    count: 0,
+  },
+  {
+    id: 8,
+    image:
+      "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/23770096/2023/7/11/757e742c-211f-4d61-9638-9632ea3a08f41689060493554-WROGN-ACTIVE-Men-Shorts-2711689060493079-1.jpg",
+    brandName: "worng ",
+    type: "short",
+    price: "588",
+    count: 0,
+  },
+  {
+    id: 9,
+    image: "https://www.beyours.in/cdn/shop/products/6.jpg?v=1669892187",
+    brandName: "The Air shorts",
+    type: "short",
+    price: "799",
+    count: 0,
+  },
+  {
+    id: 10,
+    image:
+      "https://static05.jockey.in/c_scale,h_394,w_296/jockey/uploads/dealimages/10914/originalimages/khaki-printed-woven-bermuda-us88-12.jpg",
+    brandName: "Jockey",
+    type: "short",
+    price: "599",
+    count: 0,
+  },
+  // JEANS FOR MEN
+  {
+    id: 11,
+    image:
+      "https://i.pinimg.com/1200x/7d/5a/e5/7d5ae56b2fbe7f86a2f604efe818aa33.jpg",
+    brandName: "Bagger trousers",
+    type: "jean",
+    price: "899",
+    count: 0,
+  },
+  {
+    id: 12,
+    image:
+      "https://img.tatacliq.com/images/i8/437Wx649H/MP000000013583795_437Wx649H_202206260250281.jpeg",
+    brandName: "louis philipee",
+    type: "jean",
+    price: "1299",
+    count: 0,
+  },
+  {
+    id: 13,
+    image: "https://images.meesho.com/images/products/277015117/ajrle_512.webp",
+    brandName: "Baggy jeans",
+    type: "jean",
+    price: "973",
+    count: 0,
+  },
+  {
+    id: 14,
+    image: "https://images.meesho.com/images/products/331881425/opj4i_256.webp",
+    brandName: "Petter Inland",
+    type: "jean",
+    price: "1499",
+    count: 0,
+  },
+  {
+    id: 15,
+    image:
+      "https://sslimages.shoppersstop.com/sys-master/images/h24/h43/16442085015582/A20VOJNE348MBL_MID_BLUE_alt1.jpg_2000Wx3000H",
+    brandName: "Voi jeans",
+    type: "jean",
+    price: "999",
+    count: 0,
+  },
+  {
+    id: 16,
+    image:
+      "https://m.media-amazon.com/images/I/81cPIB1bXeL._AC_UF894,1000_QL80_.jpg",
+    brandName: "Bywenzai",
+    type: "jena",
+    price: "2012",
+    count: 0,
+  },
+
+  //JEANS FOR WOMAN
+  {
+    id: 17,
+    image:
+      "https://styleunion.in/cdn/shop/products/LFJ00050MIDSTONE_1.jpg?v=1693579123&width=2048",
+    brandName: " Rise Jean",
+    type: "women jean",
+    price: "786",
+    count: 0,
+  },
+  {
+    id: 18,
+    image:
+      "https://imgmedia.lbb.in/media/2023/06/64918121a75f67498334c0ee_1687257377932.jpg",
+    brandName: "LBB",
+    type: "women jean",
+    price: "1877",
+    count: 0,
+  },
+  {
+    id: 19,
+    image:
+      "https://styleunion.in/cdn/shop/products/LFJ00067MIDSTONE_2.jpg?v=1693569725&width=2048",
+    brandName: "union of denim",
+    type: "woman jean",
+    price: "2100",
+    count: 0,
+  },
+  {
+    id: 20,
+    image:
+      "https://i1.adis.ws/i/lucky/7W15822_420_3/LUCKY-LEGEND-MID-RISE-BOY-420?sm=aspect&aspect=2:3&w=437&qlt=100",
+    brandName: "the lucky denim ",
+    type: "woman jean",
+    price: "1599",
+    count: 0,
+  },
+  //WATCH FOR ROLEX
+  {
+    id: 21,
+    image:
+      "https://5.imimg.com/data5/AK/BS/MI/ANDROID-64079611/product-jpeg.jpg",
+    brandName: "rolex golden ",
+    type: "watch",
+    price: "5500",
+    count: 0,
+  },
+  {
+    id: 22,
+    image:
+      "https://static.toiimg.com/thumb/msid-71011251,width-1280,resizemode-4/71011251.jpg",
+    brandName: "Rolex Daytona Rainbow (virat brand)",
+    type: "watch",
+    price: "450000",
+    count: 0,
+  },
+  {
+    id: 23,
+    image:
+      "https://5.imimg.com/data5/ANDROID/Default/2022/7/XN/DB/IV/157084426/product-jpeg-500x500.jpg",
+    brandName: "multicolor rolex luxury",
+    type: "watch",
+    price: "9999",
+    count: 0,
+  },
+  //NORMAL WATCHES
+  {
+    id: 24,
+    image:
+      "https://5.imimg.com/data5/SELLER/Default/2021/12/DM/WA/GY/120750372/analog-watch.jpg",
+    brandName: "analogue",
+    type: "watch",
+    price: "1999",
+    count: 0,
+  },
+  {
+    id: 25,
+    image:
+      "https://rukminim2.flixcart.com/image/850/1000/kcw9w280/watch/u/v/t/2593ym02-titan-original-imaftxehckfhvbse.jpeg?q=90",
+    brandName: "titan",
+    type: "watch",
+    price: "1576",
+    count: 0,
+  },
+  // SMART WATCH
+  {
+    id: 26,
+    image: "https://staticimg.titan.co.in/Fastrack/Catalog/38074AP01_1.jpg",
+    brandName: "fastrack",
+    type: "smart watch",
+    price: "2500",
+    count: 0,
+  },
+  {
+    id: 27,
+    image:
+      "https://media.croma.com/image/upload/v1689334134/Croma%20Assets/Communication/Wearable%20Devices/Images/273834_0_htnvva.png",
+    brandName: "noise",
+    type: "smart watch",
+    price: "1999",
+    count: 0,
+  },
+  {
+    id: 28,
+    image: "https://m.media-amazon.com/images/I/61GsL4ZiVeL.jpg",
+    brandName: "crossBeats",
+    type: "smart watch",
+    price: "1299",
+    count: 0,
+  },
+  {
+    id: 29,
+    image:
+      "https://rukminim2.flixcart.com/image/450/500/kfeamq80/smartwatch/j/t/m/ios-m00a3hn-a-apple-original-imafvvgfmagzv3gg.jpeg?q=90&crop=false",
+    brandName: "apple",
+    type: "samrt watch",
+    price: "40910",
+    count: 0,
+  },
+  {
+    id: 30,
+    image:
+      "https://assets.sangeethamobiles.com/product_img/14414/1692254140_7Eu.jpg",
+    brandName: "firebolt",
+    type: "smart watch",
+    price: "2200",
+    count: 0,
+  },
+  {
+    id: 31,
+    image:
+      "https://www.pebblecart.com/cdn/shop/products/ForestGreen.jpg?v=1672399002000&width=412",
+    brandName: "pebble cosmos",
+    type: "samrt watch",
+    price: "2600",
+    count: 0,
+  },
+  //SHOES
+  {
+    id: 32,
+    image:
+      "https://www.jiomart.com/images/product/original/rvrgwpjvsp/bruton-trendy-sports-shoes-for-men-blue-product-images-rvrgwpjvsp-0-202209021256.jpg?im=Resize=(1000,1000)",
+    brandName: "bruton",
+    type: "shoes",
+    price: "800",
+    count: 0,
+  },
+  {
+    id: 33,
+    image:
+      "https://rukminim2.flixcart.com/image/450/500/xif0q/shoe/7/2/m/6-tm-12-6-trm-white-original-imagjqyzz8z9jrgf.jpeg?q=90&crop=false",
+    brandName: "aadi",
+    type: "shoes",
+    price: "999",
+    count: 0,
+  },
+  {
+    id: 34,
+    image:
+      "https://assets.adidas.com/images/w_600,f_auto,q_auto/ba2659b7dd1a4e65812faf51009cb58c_9366/Pureboost_22_Shoes_White_HP9019_01_standard.jpg",
+    brandName: "adidas",
+    type: "shoes",
+    price: "4800",
+    count: 0,
+  },
+  {
+    id: 35,
+    image:
+      "https://media.istockphoto.com/id/172417586/photo/elegant-black-leather-shoes.jpg?s=612x612&w=0&k=20&c=c_tTljwbu2m0AGxwb27NxCgG0Y2Cv-C4v8q6V36RYbw=",
+    brandName: "elephant black leather",
+    type: "shoes",
+    price: "1399",
+    count: 0,
+  },
+  {
+    id: 36,
+    image:
+      "https://lajolla.com/wp-content/uploads/2022/01/Best-Shoe-Stores-San-Diego-1024x576.jpg",
+    brandName: "san diego",
+    type: "shoes",
+    price: "1799",
+    count: 0,
+  },
+  {
+    id: 37,
+    image:
+      "https://media.wired.com/photos/6154ba291b38af32f7638ffd/4:3/w_1747,h_1310,c_limit/Gear-Barefoot-Shoes-Freet-Tanga-SOURCE-Freet.jpg",
+    brandName: "barefoot",
+    type: "shoes",
+    price: "1466",
+    count: 0,
+  },
+  {
+    id: 38,
+    image:
+      "https://rukminim1.flixcart.com/image/450/500/xif0q/shoe/k/w/w/-original-imagrnddgwysrka2.jpeg?q=90&crop=false",
+    brandName: "spark",
+    type: "shoes",
+    price: "1600",
+    count: 0,
+  },
+  {
+    id: 39,
+    image:
+      "https://hips.hearstapps.com/hmg-prod/images/hoka-zinal-13085-1643565794.jpg?crop=1.00xw:0.752xh;0,0.115xh&resize=1200:*",
+    brandName: "hoka",
+    type: "shoes",
+    price: "9000",
+    count: 0,
+  },
+  {
+    id: 40,
+    image:
+      "https://cdn0.desidime.com/cdn-cgi/image/fit=contain,f=auto,onerror=redirect,w=1200,h=675,q=90/attachments/photos/848613/original/ezgif-3-1120c26989.jpg",
+    brandName: "DesiDime",
+    type: "shoes",
+    price: "14000",
+    count: 0,
+  },
+  //PERFUME
+  {
+    id: 41,
+    image:
+      "https://www.wildstone.in/cdn/shop/files/hydra100ml_5.jpg?v=1695880074",
+    brandName: "wildStone",
+    type: "perfume",
+    price: "299",
+    count: 0,
+  },
+  {
+    id: 42,
+    image:
+      "https://bellavitaorganic.com/cdn/shop/files/DATEWOMAN.jpg?v=1693290911&width=416",
+    brandName: "bella vita",
+    type: "perfume",
+    price: "515",
+    count: 0,
+  },
+  {
+    id: 43,
+    image:
+      "https://denverformen.com/cdn/shop/products/2_0ec34cbf-34f8-4f00-8224-c2a8a6abba4a_800x.jpg?v=1696932709",
+    brandName: "denver",
+    type: "perfume",
+    price: "525",
+    count: 0,
+  },
+  {
+    id: 44,
+    images:
+      "https://bellavitaorganic.com/cdn/shop/files/Skai_2e0d54be-3ab0-458b-a2af-47d666c93e0e.jpg?v=1693289226",
+    brandName: "bella vita",
+    type: "perfume",
+    price: "449",
+    count: 0,
+  },
+  {
+    id: 45,
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/O80005/yardley-london-morning-dew-daily-wear-perfume-100-ml-2-1641794413.jpg",
+    brandName: "yardley",
+    type: "perfume",
+    price: "650",
+    count: 0,
+  },
+  // SHIRTS
+  {
+    id: 46,
+    image:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1862801/2018/2/9/11518155061506-Roadster-Men-Maroon--Navy-Blue-Regular-Fit-Checked-Casual-Shirt-8861518155061131-1.jpg",
+    brandName: "roadster",
+    type: "shirt",
+    price: "495",
+    count: 0,
+  },
+  {
+    id: 47,
+    image:
+      "https://www.beyoung.in/api/cache/catalog/products/new_shirt_upload_21_10_2022/deep_olive_green_corduroy_shirt_for_men_base_3_11_2022_700x933.jpg",
+    brandName: "Urban shirt",
+    type: "shirt",
+    price: "1199",
+    count: 0,
+  },
+  {
+    id: 48,
+    image:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/1353004/2018/9/18/a8acac3a-077a-400f-a1c7-519215a5265e1537251707732-Roadster-Men-Blue-Denim-Washed-Casual-Shirt-5281537251705552-1.jpg",
+    brandName: "Roadster",
+    type: "shirt",
+    price: "539",
+    count: 0,
+  },
+  {
+    id: 49,
+    image:
+      "https://img.tatacliq.com/images/i8/437Wx649H/MP000000014694409_437Wx649H_202209291937211.jpeg",
+    brandName: "Royal Enflied blue",
+    type: "shirt",
+    price: "4000",
+    count: 0,
+  },
+  {
+    id: 50,
+    image:
+      "https://assets.ajio.com/medias/sys_master/root/20230602/BHbE/6479732342f9e729d714c57b/-473Wx593H-462323964-green-MODEL.jpg",
+    brandName: "DENNISLINGO PREMIUM ATTIRE",
+    type: "shirt",
+    price: "555",
     count: 0,
   },
 ];
